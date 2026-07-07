269bb296cb7933a1bc01bc62f29aad4b20d06331
KNODRON <juan.jimenezv08@gmail.com>
2026-07-07 12:30:56 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,16 @@
-.
+const CONFIG = {
+  centroInicial: [-33.4489, -70.6693],
+  zoomInicial: 13,
+  referenciaDefault: "Hora Oficial",
+  firma: "TEC. FORENSE - DEPTO. OS-9"
+};
+
+const ICONOS_MOVILIDAD = {
+  caminando: "🚶",
+  automovil: "🚗",
+  moto: "🏍️",
+  bicicleta: "🚲",
+  bus: "🚌",
+  taxi: "🚕",
+  metro: "🚇"
+};
